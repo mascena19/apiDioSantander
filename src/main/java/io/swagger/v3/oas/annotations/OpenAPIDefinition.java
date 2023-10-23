8122d8377b25d2ee385e7d6e1f6082c7733da8cc
masce <mascena19@gmail.com>
2023-10-22 23:04:09 -0300

    Projeto Final

---
+++ src/main/java/io/swagger/v3/oas/annotations/OpenAPIDefinition.java
@@ -0,0 +1,9 @@
+package io.swagger.v3.oas.annotations;
+
+import io.swagger.v3.oas.annotations.servers.Server;
+
+public @interface OpenAPIDefinition {
+
+	Server[] servers();
+
+}
---
+++ src/main/java/io/swagger/v3/oas/annotations/servers/Server.java
@@ -0,0 +1,9 @@
+package io.swagger.v3.oas.annotations.servers;
+
+public @interface Server {
+
+	String url();
+
+	String description();
+
+}
